70288c57dddd19c4cdae180b962c3dd6f9547335
oasynnoum <oasynnoum@PS2.(none)>
2012-11-03 12:28:05 +0900

    modify arduino.php

--- examples/arduino/arduino.php
+++ examples/arduino/arduino.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * this script can run with arduino board.
- * the arduino board must be installed the sketch which is provided by ruby-serialport.
- * see https://rubygems.org/gems/serialport
+ * このスクリプトの実行には arduino が必要です。
+ * このスクリプトと一緒にバンドルされている ardu.ino スケッチが arduino に
+ * アップロードされている必要があります。
  */
 
 if ($argc != 2) {
@@ -15,38 +15,29 @@ EOU;
     exit(1);
 }
 
-//$pins_asc = array(chr(55), chr(56), chr(57), chr(58), chr(59), chr(60), chr(61));
-$pins_asc = array('7', '8', '9', ':', ';', '<', '=');
-$pins_desc = array_reverse($pins_asc);
+//$pins = array(chr(55), chr(56), chr(57), chr(58), chr(59), chr(60), chr(61));
+$pins = array('7', '8', '9', ':', ';', '<', '=');
 $port = new SerialPort($argv[1]);
 $port
   ->setFlowControl(SerialPort::FLOW_CONTROL_SOFT)
   ->setBaudRate(SerialPort::BAUD_RATE_38400);
 
 $port->setCanonical(false)
-        ->setVTime(0)
-        ->setVMin(1);
-while ($port->read(1) != '?') ;
-print 'ask command detected. initiate ok.' . PHP_EOL;
-$port->setVTime(3)
+        ->setVTime(1)
         ->setVMin(0);
+print 'arduino の準備を待っています ';
+while ($port->read(1) != '?') print '.';
+print '準備完了' . PHP_EOL;
 
-$port->write(count($pins_asc) . implode('', $pins_asc));
+$output = count($pins) . implode('', $pins);
+$port->write($output);
 
 $asc = true;
 $sig = '1';
-$tmp_pins = array();
 while(true) {
-  if ($asc) {
-    $tmp_pins = $pins_asc;
-    $sig = '1';
-  } else {
-    //$tmp_pins = $pins_desc;
-    $tmp_pins = $pins_asc;
-    $sig = '0';
-  }
+  $sig = $asc ? '1' : '0';
 
-  foreach ($tmp_pins as $pin) {
+  foreach ($pins as $pin) {
     $port->write($sig . $pin); 
     usleep(50000);
   }

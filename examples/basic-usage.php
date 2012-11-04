508c32bd9010ff7929763a54740857be660c4f57
oasynnoum <oasynnoum@PS2.(none)>
2012-11-04 20:48:21 +0900

    modify basic-usage.php

--- examples/basic-usage.php
+++ examples/basic-usage.php
@@ -6,6 +6,7 @@ if ($argc != 2) {
     exit (1);
 }
 
+$status = 0;
 $portString = $argv[1];
 
 /**
@@ -17,27 +18,27 @@ $port = new SerialPort();
 
 
 try {
-    /** 
+    /* 
      * $portString が示すシリアルポートとのストリームを開きます。
      * コンストラクタ SerialPort::__construct(string $portString) でインスタンスを生成している場合は、
      * SerialPort::open(string $portString) をコールする必要はありません。
      */
     $port->open($portString);
     
-    /**
+    /*
      *  ボーレートを設定します。
      * ここではクラス定数を用いていますが整数リテラルでも構いません。
      */
     $port->setBaudRate(SerialPort::BAUD_RATE_9600);
     
-    /**
+    /*
      * フロー制御を設定します。
      * SerialPort::setFlowControl() の引数には
      * クラス定数 SerialPort::FLOW_CONTROL_* を用いてください。
      */
     $port->setFlowControl(SerialPort::FLOW_CONTROL_NONE);
     
-    /**
+    /*
      * カノニカルモードを設定します。
      * 続く SerialPort::setVMin() SerialPort::setVTime() と共に
      * データ読み込み時のタイムアウト方法を決定します。
@@ -48,14 +49,14 @@ try {
     $port->setCanonical(false)
             ->setVTime(1)->setVMin(0);
     
-    /**
-     * データを 256 バイト読み込みます。
+    /*
+     * データを 256 バイト読み込みます。引数を省略した場合1バイト読み込みます。
      * このメソッドは必ずしも指定したバイト数を読み込むとは限りません。
      * 実際に読み込んだバイト数は strlen($data) で確認できます。
      */
     $data = $port->read(256);
     
-    /**
+    /*
      * $data を送信します。
      * 読み込んだデータをそのまま返すこの挙動は echo server に似ています。
      */
@@ -63,14 +64,17 @@ try {
     
 } catch (Exception $e) {
     print $e->getMessage() . PHP_EOL;
+    $status = 2;
 }
 
-/**
+/*
  * SerialPort::isOpen() はシリアルポートが開かれているかどうか調べます。
  */
 if ($port->isOpen()) {
-    /**
+    /*
      * SerialPort::close() でシリアルポートとのストリームを閉じます。
      */
     $port->close();
 }
+
+exit ($status);

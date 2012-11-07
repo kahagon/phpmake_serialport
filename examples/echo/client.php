ef9a2a98770c13e9f1bb345da1a4f9bc74d84797
oasynnoum <oasynnoum@PS2.(none)>
2012-11-07 19:54:39 +0900

    add echo example

---
+++ examples/echo/client.php
@@ -0,0 +1,35 @@
+<?php
+if ($argc < 2) {
+    print 
+        'Usage: php ' . basename(__FILE__) . ' port_string [message]' . PHP_EOL .
+        '    port_string     接続するポート' . PHP_EOL . 
+        '    message        出力する文字列' . PHP_EOL;
+    exit (1);
+}
+
+$status = 0;
+$portString = $argv[1];
+$message = $argc == 3 ? $argv[2] : "hello";
+
+$port = new SerialPort();
+
+try {
+    
+    $port->open($portString);
+    $port->setBaudRate(SerialPort::BAUD_RATE_9600)
+            ->setFlowControl(SerialPort::FLOW_CONTROL_NONE)
+            ->setCanonical(false)
+            ->setVTime(1)
+            ->setVMin(0);
+    while($port->read()) ; // void the remain data
+    print $port->read($port->write($message)) . PHP_EOL;
+} catch (Exception $e) {
+    print $e->getMessage() . PHP_EOL;
+    $status = 2;
+}
+
+if ($port->isOpen()) {
+    $port->close();
+}
+
+exit ($status);
---
+++ examples/echo/server.php
@@ -0,0 +1,45 @@
+<?php
+if ($argc != 2) {
+    print 
+        'Usage: php ' . basename(__FILE__) . ' port_string' . PHP_EOL .
+        '    port_string     接続するポート' . PHP_EOL;
+    exit (1);
+}
+
+$status = 0;
+$portString = $argv[1];
+
+$port = new SerialPort();
+
+$data = null;
+try {
+    
+    $port->open($portString);
+    $port->setBaudRate(SerialPort::BAUD_RATE_9600)
+            ->setFlowControl(SerialPort::FLOW_CONTROL_NONE)
+            ->setCanonical(false)
+            ->setVTime(0)
+            ->setVMin(1);
+    
+    while($data = $port->read(256)) {
+        print 'data received.' . PHP_EOL;
+        if ($data == '!!!') {
+            print 'exit.' . PHP_EOL;
+            break;
+        } else {
+            var_dump($data);
+            print PHP_EOL;
+            $port->write($data);
+        }
+    }
+    
+} catch (Exception $e) {
+    print $e->getMessage() . PHP_EOL;
+    $status = 2;
+}
+
+if ($port->isOpen()) {
+    $port->close();
+}
+
+exit ($status);
